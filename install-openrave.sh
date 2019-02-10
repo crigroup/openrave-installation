fc430488b2795374c05e55b4a63ae801a7f420b9
fsuarez6 <fsuarez6@gmail.com>
2019-02-10 17:02:58 +0800

    Update OSG and OpenRAVE commits

--- install-openrave.sh
+++ install-openrave.sh
@@ -13,20 +13,20 @@ echo ""
 pip install --upgrade --user sympy==0.7.1
 
 # OpenRAVE
-COMMIT=9350ebc
+RAVE_COMMIT=7c5f5e27eec2b2ef10aa63fbc519a998c276f908
 echo ""
-echo "Installing OpenRAVE 0.9 from source (Commit $COMMIT)..."
+echo "Installing OpenRAVE 0.9 from source (Commit ${RAVE_COMMIT})..."
 echo ""
 mkdir -p ~/git; cd ~/git
 git clone https://github.com/rdiankov/openrave.git
-cd openrave; git reset --hard $COMMIT
+cd openrave; git reset --hard ${RAVE_COMMIT}
 mkdir build; cd build
 if [ $(lsb_release -sr) = '14.04' ]; then
   cmake -DODE_USE_MULTITHREAD=ON -DOSG_DIR=/usr/local/lib64/ ..
 elif [ $(lsb_release -sr) = '16.04' ]; then
   cmake -DODE_USE_MULTITHREAD=ON -DOSG_DIR=/usr/local/lib64/ ..
 # to compile on ubuntu 18.04, C11 is needed.
-elif [ $(lsb_release -sr) = '18.04' ]; then  
+elif [ $(lsb_release -sr) = '18.04' ]; then
   cmake -DODE_USE_MULTITHREAD=ON -DOSG_DIR=/usr/local/lib64/ -DCMAKE_CXX_STANDARD=11 ..
 fi
 make -j `nproc`
--- install-osg.sh
+++ install-osg.sh
@@ -7,16 +7,20 @@
 #   OpenRAVE Installation Script: OpenSceneGraph
 
 # OpenSceneGraph
+OSG_COMMIT=1f89e6eb1087add6cd9c743ab07a5bce53b2f480
 echo ""
-echo "Installing OpenSceneGraph 3.4 from source..."
+echo "Installing OpenSceneGraph 3.4 from source (Commit ${OSG_COMMIT})..."
 echo ""
+
 mkdir -p ~/git; cd ~/git
-git clone https://github.com/openscenegraph/OpenSceneGraph.git --branch OpenSceneGraph-3.4
-cd OpenSceneGraph
+git clone https://github.com/openscenegraph/OpenSceneGraph.git
+cd OpenSceneGraph; git reset --hard ${OSG_COMMIT}
 mkdir build; cd build
 if [ $(lsb_release -sr) = '14.04' ]; then
   cmake ..
 elif [ $(lsb_release -sr) = '16.04' ]; then
+  cmake .. -DDESIRED_QT_VERSION=4
+elif [ $(lsb_release -sr) = '18.04' ]; then
   cmake .. -DDESIRED_QT_VERSION=4
 fi
 make -j `nproc`
