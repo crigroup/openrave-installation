a3cbc9341d8978f94e436efcbc7d29ffc6cdc7a7
fsuarez6 <fsuarez6@gmail.com>
2019-02-26 22:15:51 +0100

    remove travis_wait

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -21,40 +21,42 @@ echo ""
 echo "Requires root privileges:"
 # Update
 if [ ${UBUNTU_VER} = '14.04' ]; then
+  sudo apt-get update -q
+  sudo apt-get install -q -y --no-install-recommends software-properties-common
   # ROS Indigo repository
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
   sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
   # Additional PPAs
   sudo apt-add-repository -y ppa:imnmfotmal/libccd
 fi
-sudo apt-get update -qq
+sudo apt-get update -q
 # Programs
-sudo apt-get install -qq -y --no-install-recommends build-essential cmake doxygen       \
+sudo apt-get install -q -y --no-install-recommends build-essential cmake doxygen        \
 g++ git ipython locate lsb-release octave python-dev python-h5py python-numpy           \
 python-pip python-scipy python-setuptools python-wheel unzip wget
 if [ ${UBUNTU_VER} = '14.04' ]; then
-  sudo apt-get install -qq -y --no-install-recommends qt4-dev-tools zlib-bin
+  sudo apt-get install -q -y --no-install-recommends qt4-dev-tools zlib-bin
 elif [ ${UBUNTU_VER} = '16.04' ] || [ ${UBUNTU_VER} = '18.04' ]; then
   sudo apt-get install -y --no-install-recommends qt5-default minizip
 fi
 # Libraries
-sudo apt-get install -qq -y --no-install-recommends ann-tools libann-dev        \
+sudo apt-get install -q -y --no-install-recommends ann-tools libann-dev         \
 libassimp-dev libavcodec-dev libavformat-dev libeigen3-dev libfaac-dev          \
 libflann-dev libfreetype6-dev liblapack-dev libglew-dev libgsm1-dev             \
 libmpfi-dev  libmpfr-dev liboctave-dev libode-dev libogg-dev libpcre3-dev       \
 libqhull-dev libsoqt-dev-common libsoqt4-dev libswscale-dev libtinyxml-dev      \
 libvorbis-dev libx264-dev libxml2-dev libxvidcore-dev
 if [ ${UBUNTU_VER} = '14.04' ]; then
-  sudo apt-get install -qq -y --no-install-recommends collada-dom-dev libccd    \
+  sudo apt-get install -q -y --no-install-recommends collada-dom-dev libccd    \
   libpcrecpp0 liblog4cxx10-dev libqt4-dev
 elif [ ${UBUNTU_VER} = '16.04' ] || [ ${UBUNTU_VER} = '18.04' ]; then
-  sudo apt-get install -qq -y --no-install-recommends libccd-dev                \
+  sudo apt-get install -q -y --no-install-recommends libccd-dev                \
   libcollada-dom2.4-dp-dev liblog4cxx-dev libminizip-dev octomap-tools
 fi
 
 # Install boost
 if [ ${UBUNTU_VER} = '14.04' ] || [ ${UBUNTU_VER} = '16.04' ]; then
-    sudo apt-get install -qq -y --no-install-recommends libboost-all-dev        \
+    sudo apt-get install -q -y --no-install-recommends libboost-all-dev        \
     libboost-python-dev
 elif [ ${UBUNTU_VER} = '18.04' ]; then
     # Install boost 1.58 from source
