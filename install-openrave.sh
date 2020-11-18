ef0582d442b1a798ebd3f410513f9d01615aea5f
nicholasadr <adr.nicholas@gmail.com>
2020-11-18 11:45:09 +0800

    Upgrade openrave for ubuntu 18.04:
    - Compile with pybind instead of downgrading boost
    - Add git credentials check, set if none
    - Add pyopengl installation
    - OpenRAVE v0.53.1

--- install-openrave.sh
+++ install-openrave.sh
@@ -22,17 +22,17 @@ echo ""
 pip install --upgrade --user sympy==0.7.1
 
 # OpenRAVE
-if [ ${UBUNTU_VER} = '14.04' ] || [ ${UBUNTU_VER} = '16.04' ] || [ ${UBUNTU_VER} = '18.04' ]; then
+if [ ${UBUNTU_VER} = '14.04' ] || [ ${UBUNTU_VER} = '16.04' ]; then
 	RAVE_COMMIT=7c5f5e27eec2b2ef10aa63fbc519a998c276f908
 	echo ""
 	echo "Installing OpenRAVE 0.9 from source (Commit ${RAVE_COMMIT})..."
 	echo ""
 	mkdir -p ~/git; cd ~/git
 	git clone https://github.com/rdiankov/openrave.git
-elif [ ${UBUNTU_VER} = '20.04' ]; then
+elif [ ${UBUNTU_VER} = '18.04' ] || [ ${UBUNTU_VER} = '20.04' ]; then
 	RAVE_COMMIT=2024b03554c8dd0e82ec1c48ae1eb6ed37d0aa6e
 	echo ""
-	echo "Installing OpenRAVE 0.53 from source (Commit ${RAVE_COMMIT})..."
+	echo "Installing OpenRAVE 0.53.1 from source (Commit ${RAVE_COMMIT})..."
 	echo ""
 	mkdir -p ~/git; cd ~/git
 	git clone -b production https://github.com/rdiankov/openrave.git
@@ -41,10 +41,7 @@ cd openrave; git reset --hard ${RAVE_COMMIT}
 mkdir build; cd build
 if [ ${UBUNTU_VER} = '14.04' ] || [ ${UBUNTU_VER} = '16.04' ]; then
   	cmake -DODE_USE_MULTITHREAD=ON -DOSG_DIR=/usr/local/lib64/ ..
-elif [ ${UBUNTU_VER} = '18.04' ]; then
-  	cmake -DODE_USE_MULTITHREAD=ON -DCMAKE_CXX_STANDARD=11            \
-  		-DBoost_NO_SYSTEM_PATHS=TRUE -DBOOST_ROOT=/usr/local/ ..
-elif [ ${UBUNTU_VER} = '20.04' ]; then
+elif [ ${UBUNTU_VER} = '18.04' ] || [ ${UBUNTU_VER} = '20.04' ]; then
   	cmake -DODE_USE_MULTITHREAD=ON -DOSG_DIR=/usr/local/lib64/ \
   		-DUSE_PYBIND11_PYTHON_BINDINGS:BOOL=TRUE 			   \
   		-DBoost_NO_BOOST_CMAKE=1 ..
